12888d52797960d69e3cc31c1cb1b6c0832cdcb1
ParkhomenkoDV <parkho.m.enko@mail.ru>
2024-09-28 20:16:27 +0300

    2024-09-28-20-16

--- setup.py
+++ setup.py
@@ -21,5 +21,8 @@ setup(
     url='https://github.com/ParkhomenkoDV/airfoil.git',
     packages=[name],
     python_requires='>=3.8',
-    install_requires=install_requires,
+    install_requires=['tqdm', 'colorama', 'numpy', 'scipy', 'matplotlib', 'pandas',
+                      'git+https://github.com/ParkhomenkoDV/decorators.git@master',
+                      'git+https://github.com/ParkhomenkoDV/tools.git@master',
+                      'git+https://github.com/ParkhomenkoDV/mathematics.git@master', ],
 )
